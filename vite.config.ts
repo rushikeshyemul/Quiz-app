4cd44b3609f2d7b0e0b02ca84bb0f37b85ed4802
Rushikesh Yemul <yemulrusikesh56@gmail.com>
2025-10-08 15:35:44 +0530

    updated url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Proxy API requests to backend
+      "/api": "https://quiz-app-backend-o3i5.onrender.com", // Proxy API requests to backend
     },
   },
 });
